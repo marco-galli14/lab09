c495e15e67b868cd6853ccf7c0f84ef10fe6f16e
Marco Galli - marco.galli14@studio.unibo.it <marco.galli14@studio.unibo.it>
2025-11-17 16:55:02 +0100

    Finire SimpleGUI e SimpleGUIWithFileChooser

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,52 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private static final String DEFAULT = System.getProperty("user.home")
+        + System.getProperty("file.separator")
+        + "output.txt";
+    
+    private File file;
+
+    /**
+     * The constructor sets the file as default "output.txt"
+     */
+    public Controller() {
+        this.file = new File(DEFAULT);
+    }
+
+    /**
+     * Sets the current file of the Controller as "corrente"
+     * 
+     * @param corrente the new current File of the Controller
+     */
+    public void setFile(final File corrente) {
+        this.file = corrente;
+    }
+
+    public File getFile() {
+        return this.file;
+    }
+
+    public Path getPath() {
+        return this.file.toPath();
+    }
+
+    public void writeString(final String parola) {
+        try (PrintStream o = new PrintStream(this.file, StandardCharsets.UTF_8)) {
+            o.print(parola);
+        } catch (IOException e) {
+            System.err.println(e); // NOPMD
+        }
+    }
+    
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,12 @@
 package it.unibo.mvc;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +14,35 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private static final int PROPORTION = 3;
+
     private final JFrame frame = new JFrame();
+    private final Controller controller = new Controller();
+
+    public SimpleGUI() {
+        final JPanel pano = new JPanel();
+        frame.setLayout(new BorderLayout());
+        final JTextArea testo = new JTextArea();
+        final JButton save = new JButton("Save");
+        frame.setContentPane(pano);
+        pano.add(testo);
+        pano.add(save, BorderLayout.SOUTH);
+
+        save.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.writeString(testo.getText());
+            }
+            
+        });
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI();
+    }
 
 }
